84ac407d423bac81494dde23861ad31789768852
Keisuke Miyako <miyako@wakanda.jp>
2016-08-30 23:43:55 +0900

    added $4, not using temp file on mac

--- direct-print/4DPlugin.cpp
+++ direct-print/4DPlugin.cpp
@@ -52,11 +52,13 @@ void PRINT_BLOB(sLONG_PTR *pResult, PackagePtr pParams)
 {
 	C_TEXT Param1;
 	C_LONGINT Param3;
+	C_TEXT Param4;
 	
 	PA_Handle h = *(PA_Handle *)(pParams[1]);
 	if(h)
 	{
 		Param1.fromParamAtIndex(pParams, 1);
+		Param4.fromParamAtIndex(pParams, 4);
 		
 #if VERSIONWIN
 		
@@ -71,7 +73,13 @@ void PRINT_BLOB(sLONG_PTR *pResult, PackagePtr pParams)
 			DOC_INFO_1 docInfo;
 			docInfo.pDocName = L"DOCUMENT";
 			docInfo.pOutputFile = NULL;
-			docInfo.pDatatype = L"RAW";
+			if(!Param4.getUTF16Length())
+			{
+				docInfo.pDatatype = L"RAW";
+			}else{
+				docInfo.pDatatype = (LPTSTR)Param4.getUTF16StringPtr();
+			}
+			
 			DWORD printJob = StartDocPrinter(&printer, (DWORD)1, (LPBYTE)&docInfo);
 			
 			if(printJob)
@@ -106,6 +114,14 @@ void PRINT_BLOB(sLONG_PTR *pResult, PackagePtr pParams)
 			Param3.setIntValue(ERR_OPEN_PRINTER);
 		}
 #else
+		
+		NSString *mimeType = @"application/vnd.cups-raw";
+		
+		if(Param4.getUTF16Length())
+		{
+			mimeType = Param4.copyUTF16String();
+		}
+		
 		NSString *printerName = Param1.copyUTF16String();
 		CFArrayRef printers;
 		
@@ -148,40 +164,43 @@ void PRINT_BLOB(sLONG_PTR *pResult, PackagePtr pParams)
 											status = PMPrinterGetMimeTypes(printer, settings, &mimeTypes);
 											if (status == noErr && mimeTypes)
 											{
-												if (CFArrayContainsValue(mimeTypes, CFRangeMake(0, CFArrayGetCount(mimeTypes)), @"application/vnd.cups-raw"))
+												if (CFArrayContainsValue(mimeTypes, CFRangeMake(0, CFArrayGetCount(mimeTypes)), mimeType))
 												{
 													char* buf = PA_LockHandle(h);
 													PA_long32 len = PA_GetHandleSize(h);
 													
 													NSData *data = [[NSData alloc]initWithBytes:(const void *)buf length:len];
 													
-													NSString *temporaryFolder = NSTemporaryDirectory();
+//													NSString *temporaryFolder = NSTemporaryDirectory();
 									
-#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 1080
-													NSString *uuid = [[[NSUUID UUID]UUIDString]stringByReplacingOccurrencesOfString:@"-" withString:@""];
-#else
-													CFUUIDRef _uuid = CFUUIDCreate(kCFAllocatorDefault);
-													NSString *uuid = [(NSString *)CFUUIDCreateString(kCFAllocatorDefault, _uuid) stringByReplacingOccurrencesOfString:@"-" withString:@""];
-													CFRelease(_uuid);
-#endif
-													NSString *path = [[temporaryFolder stringByAppendingPathComponent:uuid]stringByAppendingPathExtension:@"zpl"];
+//#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 1080
+//													NSString *uuid = [[[NSUUID UUID]UUIDString]stringByReplacingOccurrencesOfString:@"-" withString:@""];
+//#else
+//													CFUUIDRef _uuid = CFUUIDCreate(kCFAllocatorDefault);
+//													NSString *uuid = [(NSString *)CFUUIDCreateString(kCFAllocatorDefault, _uuid) stringByReplacingOccurrencesOfString:@"-" withString:@""];
+//													CFRelease(_uuid);
+//#endif
+//													NSString *path = [[temporaryFolder stringByAppendingPathComponent:uuid]stringByAppendingPathExtension:@"zpl"];
 													
-													if([data writeToFile:path atomically:YES])
+//													if([data writeToFile:path atomically:YES])
+//													{
+//														NSURL *url = [[NSURL alloc]initFileURLWithPath:path isDirectory:NO];
+//														if(url)
+//														{
+													
+													CGDataProviderRef provider = CGDataProviderCreateWithCFData((CFDataRef)data);
+													status = PMPrinterPrintWithProvider(printer, settings, format, (CFStringRef)mimeType, provider);
+													CGDataProviderRelease(provider);
+													
+//															status = PMPrinterPrintWithFile(printer, settings, format, (CFStringRef)mimeType, (CFURLRef)url);
+//															[url release];
+													if(status)
 													{
-														NSURL *url = [[NSURL alloc]initFileURLWithPath:path isDirectory:NO];
-														if(url)
-														{
-															status = PMPrinterPrintWithFile(printer, settings, format, CFSTR("application/vnd.cups-raw"), (CFURLRef)url);
-															[url release];
-															if(!status)
-															{
-																NSLog(@"path:%@", path);
-															}else{
-																Param3.setIntValue(ERR_PM_PRINTER_PRINT_WITH_FILE);
-															}
-														}
-														[[NSFileManager defaultManager]removeItemAtPath:path error:NULL];
+														Param3.setIntValue(ERR_PM_PRINTER_PRINT_WITH_FILE);
 													}
+//														}
+//														[[NSFileManager defaultManager]removeItemAtPath:path error:NULL];
+//													}
 													[data release];
 												}
 											}
@@ -212,6 +231,11 @@ void PRINT_BLOB(sLONG_PTR *pResult, PackagePtr pParams)
 			[printerName release];
 			CFRelease(printers);
 		}
+		
+		if(Param4.getUTF16Length())
+		{
+			[mimeType release];
+		}
 #endif
 	}
 
@@ -223,6 +247,7 @@ void PRINT_BLOB_ARRAY(sLONG_PTR *pResult, PackagePtr pParams)
 #ifndef PLUGIN_API_V11
 	C_TEXT Param1;
 	C_LONGINT Param3;
+	C_TEXT Param4;
 	
 	PA_Variable arr = *((PA_Variable*) pParams[1]);
 	
@@ -233,6 +258,7 @@ void PRINT_BLOB_ARRAY(sLONG_PTR *pResult, PackagePtr pParams)
 			if(arr.fType == eVK_ArrayBlob)
 			{
 				Param1.fromParamAtIndex(pParams, 1);
+				Param4.fromParamAtIndex(pParams, 4);
 				
 #if VERSIONWIN
 				
@@ -247,7 +273,14 @@ void PRINT_BLOB_ARRAY(sLONG_PTR *pResult, PackagePtr pParams)
 					DOC_INFO_1 docInfo;
 					docInfo.pDocName = L"DOCUMENT";
 					docInfo.pOutputFile = NULL;
-					docInfo.pDatatype = L"RAW";
+					
+					if(!Param4.getUTF16Length())
+					{
+						docInfo.pDatatype = L"RAW";
+					}else{
+						docInfo.pDatatype = (LPTSTR)Param4.getUTF16StringPtr();
+					}
+					
 					DWORD printJob = StartDocPrinter(&printer, (DWORD)1, (LPBYTE)&docInfo);
 					
 					if(printJob)
@@ -288,6 +321,14 @@ void PRINT_BLOB_ARRAY(sLONG_PTR *pResult, PackagePtr pParams)
 					Param3.setIntValue(ERR_OPEN_PRINTER);
 				}
 #else
+				
+				NSString *mimeType = @"application/vnd.cups-raw";
+				
+				if(Param4.getUTF16Length())
+				{
+					mimeType = Param4.copyUTF16String();
+				}
+				
 				NSString *printerName = Param1.copyUTF16String();
 				CFArrayRef printers;
 				
@@ -330,7 +371,7 @@ void PRINT_BLOB_ARRAY(sLONG_PTR *pResult, PackagePtr pParams)
 													status = PMPrinterGetMimeTypes(printer, settings, &mimeTypes);
 													if (status == noErr && mimeTypes)
 													{
-														if (CFArrayContainsValue(mimeTypes, CFRangeMake(0, CFArrayGetCount(mimeTypes)), @"application/vnd.cups-raw"))
+														if (CFArrayContainsValue(mimeTypes, CFRangeMake(0, CFArrayGetCount(mimeTypes)), mimeType))
 														{
 															
 															PA_long32 size = PA_GetArrayNbElements(arr);
@@ -342,33 +383,35 @@ void PRINT_BLOB_ARRAY(sLONG_PTR *pResult, PackagePtr pParams)
 																
 																NSData *data = [[NSData alloc]initWithBytes:(const void *)buf length:len];
 																
-																NSString *temporaryFolder = NSTemporaryDirectory();
+//																NSString *temporaryFolder = NSTemporaryDirectory();
 																
-#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 1080
-																NSString *uuid = [[[NSUUID UUID]UUIDString]stringByReplacingOccurrencesOfString:@"-" withString:@""];
-#else
-																CFUUIDRef _uuid = CFUUIDCreate(kCFAllocatorDefault);
-																NSString *uuid = [(NSString *)CFUUIDCreateString(kCFAllocatorDefault, _uuid) stringByReplacingOccurrencesOfString:@"-" withString:@""];
-																CFRelease(_uuid)
-#endif
-																NSString *path = [[temporaryFolder stringByAppendingPathComponent:uuid]stringByAppendingPathExtension:@"zpl"];
+//#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 1080
+//																NSString *uuid = [[[NSUUID UUID]UUIDString]stringByReplacingOccurrencesOfString:@"-" withString:@""];
+//#else
+//																CFUUIDRef _uuid = CFUUIDCreate(kCFAllocatorDefault);
+//																NSString *uuid = [(NSString *)CFUUIDCreateString(kCFAllocatorDefault, _uuid) stringByReplacingOccurrencesOfString:@"-" withString:@""];
+//																CFRelease(_uuid)
+//#endif
+//																NSString *path = [[temporaryFolder stringByAppendingPathComponent:uuid]stringByAppendingPathExtension:@"zpl"];
 																
-																if([data writeToFile:path atomically:YES])
-																{
-																	NSURL *url = [[NSURL alloc]initFileURLWithPath:path isDirectory:NO];
-																	if(url)
-																	{
-																		status = PMPrinterPrintWithFile(printer, settings, format, CFSTR("application/vnd.cups-raw"), (CFURLRef)url);
-																		[url release];
-																		if(!status)
+//																if([data writeToFile:path atomically:YES])
+//																{
+//																	NSURL *url = [[NSURL alloc]initFileURLWithPath:path isDirectory:NO];
+//																	if(url)
+//																	{
+																CGDataProviderRef provider = CGDataProviderCreateWithCFData((CFDataRef)data);
+																status = PMPrinterPrintWithProvider(printer, settings, format, (CFStringRef)mimeType, provider);
+																CGDataProviderRelease(provider);
+																
+//																status = PMPrinterPrintWithFile(printer, settings, format, CFSTR("application/vnd.cups-raw"), (CFURLRef)url);
+//																		[url release];
+																		if(status)
 																		{
-																			NSLog(@"path:%@", path);
-																		}else{
 																			Param3.setIntValue(ERR_PM_PRINTER_PRINT_WITH_FILE);
 																		}
-																	}
-																	[[NSFileManager defaultManager]removeItemAtPath:path error:NULL];
-																}
+//																	}
+//																	[[NSFileManager defaultManager]removeItemAtPath:path error:NULL];
+//																}
 																[data release];
 
 																PA_UnlockHandle(blob.fHandle);
@@ -401,6 +444,11 @@ void PRINT_BLOB_ARRAY(sLONG_PTR *pResult, PackagePtr pParams)
 					}
 					[printerName release];
 					CFRelease(printers);
+				}
+				
+				if(Param4.getUTF16Length())
+				{
+					[mimeType release];
 				}
 #endif
 			}
